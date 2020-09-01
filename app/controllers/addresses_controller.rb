253ee33bc3d7a3b68d8e1561b538e43c6bf9656d
@okadayuuki <oy19950908@gmail.com>
2020-09-01 14:30:38 +0900

    配送先の編集、削除の実装

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,32 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def create
+    address = Address.new(address_params)
+    address.save
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
+
   end
 
   def update
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:poatal_code, :address, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,7 @@ Rails.application.routes.draw do
   get 'home/top' => 'home/top',as: 'member_top'
   get 'home/about' => 'home/about',as: 'member_about'
 
+  	resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
