d56fceb8983da2a9c794005370fb134bbb0559e0
Shinozaki <67033677+shinogit@users.noreply.github.com>
2020-09-03 15:08:03 +0900

    Update routes.rb
    ルーティング修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,29 @@
 Rails.application.routes.draw do
 
-#会員側のルーティング#
+#会員側のルーティング
   devise_for :members
-
   namespace :members do
-
     get 'home/top' => 'home/top',as: 'member_top'
     get 'home/about' => 'home/about',as: 'member_about'
     get "members/my_page" => "members#show"
     get "members/unsubscribe" => "members#unsubscribe"
     get "members/withdraw" => "members#withdraw"
-
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-
     resources :members, only: [:edit, :update, :show]
-
     resources :items, only: [:index, :show]
-
     resources :genres, only: [:show]
-
   end
 
+  #顧客側のルーティング
   devise_for :admins
-
   namespace :admins do
     get '' => 'tops#top'
-  end
-  namespace :admins do
     resources :orders, only: [:index, :show, :update]
-  end
-  namespace :admins do
     resources :order_items, only: [:update]
-  end
-  namespace :admins do
     resources :items, only: [:index, :show, :edit, :update, :create, :new]
-  end
-  namespace :admins do
     resources :genres, only: [:index, :edit, :update, :create]
-  end
-  namespace :admins do
     resources :members, only: [:index, :show, :edit, :update]
   end
-
-
-
-  #会員側のルーティング#
-
-  devise_for :members
-  get "members/my_page" => "members#show"
-  get "members/unsubscribe" => "members#unsubscribe"
-  get "members/withdraw" => "members#withdraw"
-  get 'home/top' => 'home/top',as: 'member_top'
-  get 'home/about' => 'home/about',as: 'member_about'
-    resources :members, only:[:edit, :update]
-
-  	resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-
-  	resources :items, only: [:index, :show]
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
