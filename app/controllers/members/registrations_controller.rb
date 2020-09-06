24c4f9c9b205d60cf9259cee38a25e2794fc626a
篠崎裕太 <shnzk@ShinozMacBook.local>
2020-09-06 17:32:30 +0900

    [Edit] members/registrations_controllerの体裁整えました。

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-class Members::RegistrationsController < Devise::RegistrationsController
+class Members::SessionsController < Devise::SessionsController
   layout 'members'
-before_action :configure_permitted_parameters, if: :devise_controller?
-
-
-  def after_sign_in_path_for(resource)
-      items_path
-  end
-
-
-  def after_sign_out_path_for(resource)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    def after_sign_in_path_for(resource)
+      home_top_path
+    end
+  
+    def after_sign_out_path_for(resource)
       new_member_session_path
-  end
+    end
+    
 
+    protected
 
-  protected
-
-  def configure_permitted_parameters
+    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :last_name,
       :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted])# 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
       devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password, :last_name,
       :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
-  end
-#オカタクメモ1：上記のcofigure(以下ストロングパラメーターが分かりやすいのでそう呼ぶ)以下の記載がないとunpermitted.parameters: :カラム名とコマンドプロンプトに表れていた。
-#実際にパラメータの受け渡しがされていなかったので、次のページでは受け取ったパラメータが表示されるはずが空白になっていた。ようこそ～さん！の～の部分のこと。
+    end
+  #オカタクメモ1：上記のcofigure(以下ストロングパラメーターが分かりやすいのでそう呼ぶ)以下の記載がないとunpermitted.parameters: :カラム名とコマンドプロンプトに表れていた。
+  #実際にパラメータの受け渡しがされていなかったので、次のページでは受け取ったパラメータが表示されるはずが空白になっていた。ようこそ～さん！の～の部分のこと。
+
+  
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
